ebdba51ea266a2675d066acec5f36e63015e5255
emyguidi07 <emy.guidi.cavalcante2020@gmail.com>
2022-09-16 07:24:46 -0300

    exercicio com styled components

--- exercicio2/App.js
+++ exercicio2/App.js
@@ -1,46 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import react from 'react';
-import {  Text, View, TextInput, Button, Image} from 'react-native';
+import React from 'react';
 import img from '../exercicio2/assets/img/react.jpg';
-import styled from 'styled-components/native';
+import { Container, Title, Img, Botao, Form} from './styles';
 
-const Container = styled.View`
-  flex: 1;
-  background-color: #fff;
-  align-items: center;
-  justify-content: center;
-`; 
-
-const Title = styled.Text`
-color: "blue";
-text-align: center;
-font-size: 30px;
-`;
-
-const Form = styled.TextInput`
-    height: 40;
-    borderColor: "gray";
-    borderWidth: 1;
-    padding: 5;
-    margin: 15;
-`;
-
-const Botao = styled.Button`
-height: 40;
-background-Color: "blue";
-borderWidth: 1;
-color: #fff;
-font-size: 1em;
-padding: 0.25em 1em;
-margin: 15;
-`;
-
-const Img = styled.Image`
-style={{width: 200;
-height: 210;
-alignSelft: "center";
-`;
-export default () => (
+export default function App() {
+  return(
     <Container>
       <Title>Login</Title>
       <Form>Digite seu email!</Form>
@@ -49,4 +12,4 @@ export default () => (
       <Img source = {{uri: img}}></Img>
     </Container>
   );
-
+}
---
+++ exercicio2/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native';
+
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: #fff;
+    align-items: center;
+    justify-content: center;
+`;
+export const Title = styled.Text`
+    color: "blue";
+    font-size: 20px;
+    marginHorizontal: 15;
+    marginBottom: 10;
+`;
+export const Form = styled.TextInput`
+    height: 40;
+    borderColor: "gray";
+    borderWidth: 1;
+    padding: 5;
+    margin: 15;
+`;
+export const Botao = styled.Button`
+height: 40;
+background-Color: "blue";
+borderWidth: 1;
+color: #fff;
+font-size: 1em;
+padding: 0.25em 1em;
+margin: 15;
+`;
+
+export const Img = styled.Image`
+width: 200;
+height: 210;
+alignSelft: "center";
+`;
